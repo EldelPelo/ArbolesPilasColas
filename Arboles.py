50696d0ac6f7167c4bd6a919cddb56609136393c
adrianga96 <30849869+adrianga96@users.noreply.github.com>
2017-08-16 06:55:37 -0500

    Update Arboles.py

--- Arboles.py
+++ Arboles.py
@@ -1,3 +1,56 @@
+class Nodo():
+    def __init__(self, val, izq=None, der=None):
+        self.valor = val
+        self.izq = izq
+        self.der = der
+
+class Pila:
+    def __init__(self):
+        self.pila = []
+    def agregar(self, elemento):
+        self.pila.append(elemento)
+    def sacar(self):
+        if self.vacia()==False:
+            return self.pila.pop()
+        else:
+            return "Pila vacia"
+    def vacia(self):
+        return self.pila == []
+
+def convertir(lista, pila):
+    if lista != []:
+        if lista[0] in "+-*/":
+            nodo_der = pila.sacar()
+            nodo_izq = pila.sacar()
+            pila.agregar(Nodo(lista[0],nodo_izq,nodo_der))
+        else:
+            pila.agregar(Nodo(lista[0]))
+        return convertir(lista[1:],pila)
+            
+
+def evaluar(arbol):
+    if arbol.valor == "+":
+        return evaluar(arbol.izq) + evaluar(arbol.der)
+    if arbol.valor == "-":
+        return evaluar(arbol.izq) - evaluar(arbol.der)
+    if arbol.valor == "/":
+        return evaluar(arbol.izq) / evaluar(arbol.der)
+    if arbol.valor == "*":
+        return evaluar(arbol.izq) * evaluar(arbol.der)
+    return int(arbol.valor)
+    
+
+
+def main():
+    exp = raw_input("ingrese l expresion en formato posfijo: ").split(" ")
+    pila = Pila()
+    convertir(exp, pila)
+    print evaluar(pila.sacar())
+
+main()
+
+#Hasta aca hicimos, funciona bien con posfija
+
 class Node:
     """
     Class Node
